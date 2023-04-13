bae5d5f75643bacfcc0795f2b9b75224d38bd8ef
Mohd Saif <mohdsaifb173@gmail.com>
2023-04-13 16:21:23 +0530

    changes

--- src/components/Bestseller/Bestseller.js
+++ src/components/Bestseller/Bestseller.js
@@ -1,16 +1,18 @@
 import React, { PureComponent } from "react";
+import "./Bestseller.css";
 import BS from "../../images/Bestseller(1).jpg";
 import Drink from "../../images/Drinks(2).jpg";
 import Food from "../../images/Food(3).jpg";
 import Merchendise from "../../images/Merchandise(4).jpg";
 import coffee from "../../images/CoffeeAtHome(5).jpg";
 import readytoeat from "../../images/ReadyToEat(6).jpg";
+import vegicon from "../../images/veg.svg";
 
 export class Bestseller extends PureComponent {
   render() {
     return (
       <div>
-        <div className="row justify-content-evenly">
+        <div className="row justify-content-evenly mb-5 mt-3">
           <div className="col-10 pt-4 pb-3">
             <div className="">
               <h4>Handcrafted Curations</h4>
@@ -84,9 +86,111 @@ export class Bestseller extends PureComponent {
             </div>
           </div>
         </div>
-        
 
-        
+        {/* BR-section */}
+        <div className="BR-container">
+          <div className="br-upperSection d-flex align-content-center justify-content-between">
+            <div className="br-heading">
+              <h3>Barista Recommends</h3>
+            </div>
+            <div className="view-menu">
+              <a href="#">View Menu</a>
+            </div>
+          </div>
+
+          {/* cards  */}
+          <div className="cards d-flex mt-4 mb-4 justify-content-between">
+            <div className="cards-content">
+              <div className="upper-content d-flex">
+                <div className="card-img">
+                  <img src={BS} alt="image" />
+                </div>
+                <div className="card-about">
+                  <div>
+                    <img src={vegicon} alt="veg-symbol" />
+                  </div>
+                  <div className="card-title">
+                    <h3>Java Chip Frappuccino</h3>
+                  </div>
+                  <div className="card-subtitle">
+                    <span> TALL(354 ML) </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card-bottom d-flex mt-1">
+                <div className="card-price">
+                  <h6>₹ 351</h6>
+                </div>
+                <div className="card-add-item">
+                  <button className="text-white">Add Item</button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- card2 --> */}
+            <div className="cards-content">
+              <div className="upper-content d-flex">
+                <div className="card-img">
+                  <img src={BS} alt="image" />
+                </div>
+                <div className="card-about">
+                  <div>
+                    <img src={vegicon} alt="veg-symbol" />
+                  </div>
+                  <div className="card-title">
+                    <h3>Cold coffee</h3>
+                  </div>
+                  <div className="card-subtitle">
+                    <span> TALL(354 ML) </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card-bottom d-flex mt-1">
+                <div className="card-price">
+                  <h6>₹ 351</h6>
+                </div>
+                <div className="card-add-item">
+                  <button className="text-white">Add Item</button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- card3 --> */}
+            <div className="cards-content">
+              <div className="upper-content d-flex">
+                <div className="card-img">
+                  <img src={BS} alt="image" />
+                </div>
+                <div className="card-about">
+                  <div>
+                    <img src={vegicon} alt="veg-symbol" />
+                  </div>
+                  <div className="card-title">
+                    <h3>Vanilla Sweet Cream Cold Brew</h3>
+                  </div>
+                  <div className="card-subtitle">
+                    <span> TALL(354 ML) </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card-bottom d-flex mt-1">
+                <div className="card-price">
+                  <h6>₹ 383</h6>
+                </div>
+                <div className="card-add-item">
+                  <button className="text-white">Add Item</button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- card-end --> */}
+          </div>
+
+          {/* br-card-end  */}
+        </div>
       </div>
     );
   }
--- src/components/green_header/green_header.js
+++ src/components/green_header/green_header.js
@@ -5,8 +5,8 @@ export class Green_header extends PureComponent {
   render() {
     return (
       <div>
-        <div className='green-header d-flex justify-content-around align-items-center'>
-          <div className='text-white'><h5>A world of rewards awaits you! Sign up now.</h5></div>
+        <div className='green-header d-flex justify-content-between align-items-center'>
+          <div className='text-white'><h5> Avail benefits! Sign up for My Starbucks Rewards.  </h5></div>
           <div><button >know more</button></div>
           </div>
       </div>
