8b0fbbb16b30686024fd9c3563ba40d43d6f930c
Mohd Saif <mohdsaifb173@gmail.com>
2023-05-01 18:58:03 +0530

    created pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import Header from "./components/header/header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout"
+import Home from "./pages/Home"
+import Gift from "./pages/gift"
+import Store from "./pages/store"
+import Pay from "./pages/pay"
+import Order from "./pages/order"
+import NoPage from "./pages/nopage"
+export default function Pages() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="gift" element={<Gift />} />
+          <Route path="pay" element={<Pay />} />
+          <Route path="store" element={<Store />} />
+          <Route path="order" element={<Order />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
